d7c93fb24ef1d0e7ef8d27cad7138cdbe495fba3
msaquiba <mdsaquibansari24@gmail.com>
2024-10-25 22:49:55 +0530

    added a student class

---
+++ configserver/src/main/java/entity/Student.java
@@ -0,0 +1,15 @@
+package entity;
+
+
+public class Student {
+    private String name;
+    private String address;
+    private int rollNo;
+
+    Student(String name,String address,int rollNo){
+        this.name=name;
+        this.address=address;
+        this.rollNo=rollNo;
+    }
+
+}
